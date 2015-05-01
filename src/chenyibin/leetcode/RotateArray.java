e08cb36cd54408df25c0b850abcc473b12cd0b37
Yibin Chen <chenyibin@gmail.com>
2015-05-01 00:23:07 -0700

    Submitting solution to Binary Tree Inorder Traversal

---
+++ src/chenyibin/leetcode/RotateArray.java
@@ -0,0 +1,29 @@
+package chenyibin.leetcode;
+
+/**
+ * Problem #189 on leetcode.com:
+ * @author Yibin Chen
+ */
+public class RotateArray {
+
+	public void rotate(int[] nums, int k) {
+		int lastIndex = nums.length - 1;
+		k %= nums.length;
+		int kIndex = k - 1;
+		reverseArray(nums, 0, lastIndex);
+		reverseArray(nums, 0, kIndex);
+		reverseArray(nums, k, lastIndex);
+	}
+
+	private void reverseArray(int[] nums, int start, int end)
+	{
+		while (start < end) {
+			// swap without temp
+			nums[start] ^= nums[end];
+			nums[end] ^= nums[start];
+			nums[start] ^= nums[end];
+			++start;
+			--end;
+		}
+	}
+}
