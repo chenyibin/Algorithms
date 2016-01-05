a3ccd2b555653b695ce498146975803054e21807
Yibin Chen <chenyibin@gmail.com>
2016-01-04 22:57:10 -0800

    Single Number III

---
+++ src/chenyibin/leetcode/medium/SingleNumberIII.java
@@ -0,0 +1,30 @@
+package chenyibin.leetcode.medium;
+
+/**
+ * Problem #260 on leetcode.com:
+ * Given an array of numbers nums, in which exactly two elements
+ * appear only once and all the other elements appear exactly twice.
+ * Find the two elements that appear only once. 
+ * @author Yibin Chen
+ */
+public class SingleNumberIII {
+    public int[] singleNumber(int[] nums)
+    {
+        int xor = 0;
+        for (int num : nums) {
+            xor ^= num;
+        }
+        int firstBit = xor & -xor;
+        
+        int groupSet = 0;
+        int groupUnset = 0;
+        for (int num : nums) {
+            if ((num & firstBit) == 0) {
+                groupUnset ^= num;
+            } else {
+                groupSet ^= num;
+            }
+        }
+        return new int[] {groupUnset, groupSet};
+    }
+}
